a2d82af48ebfc0a7794c3f6dedd974c457b3438d
Sammara Valente <sammaravs@outlook.com>
2022-11-10 19:56:47 -0300

    minhas anotações durante a primeira aula de JavaScript: variaveis e operadores

---
+++ 1-variaveis-operadores.js
@@ -0,0 +1,23 @@
+/*
+    Bootcamp Orange Tech+ DIO e Banco Inter.
+    Curso Primeiros Passos Com JavaScript
+    Aula 1: Variáveis e Operadores 
+*/
+
+
+/*
+~VARIAVEIS~  
+let - pode ser modificada
+const - não dá pra modificar
+*/
+
+
+/*
+~ATRIBUIÇÃO E IGUALDADE~
+= atribuição
+== igual a
+=== igual a
+*/
+
+
+// Imprimir algo no console: console.log()
